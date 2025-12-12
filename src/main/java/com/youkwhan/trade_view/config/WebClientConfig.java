a73bdc4f12ee85a5a09d7908d1c7d55de8775991
Youkwhan <youkwhan@gmail.com>
2025-12-11 21:22:55 -0500

    Add: webclient configuration for alpha vantage api

---
+++ src/main/java/com/youkwhan/trade_view/config/WebClientConfig.java
@@ -0,0 +1,18 @@
+package com.youkwhan.trade_view.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder,
+                               @Value("${alpha.vantage.base.url}") String baseUrl) {
+        return builder.baseUrl(baseUrl).build();
+    }
+
+
+}
