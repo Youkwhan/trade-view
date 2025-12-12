68ef81796c730260e4cd154db5639005e55d7c0f
Youkwhan <youkwhan@gmail.com>
2025-12-11 22:00:25 -0500

    Add: env variables

--- src/main/java/com/youkwhan/trade_view/client/StockClient.java
+++ src/main/java/com/youkwhan/trade_view/client/StockClient.java
@@ -1,6 +1,7 @@
 package com.youkwhan.trade_view.client;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -11,4 +12,6 @@ public class StockClient {
 
     private final WebClient webClient;
 
+    @Value("${alpha.vantage.api.key}")
+    private String apiKey;
 }
